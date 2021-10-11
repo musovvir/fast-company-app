0ad4c8391f6dccc426aee9a8856372930fe37065
musovvir <mr.musovvir@mail.ru>
2021-10-11 15:38:05 +0300

    router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import NavBar from "./NavBar"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Main from "./layouts/Main"
 import Users from "./layouts/Users"
 import Login from "./layouts/Login"
@@ -11,8 +11,9 @@ const App = () => {
       <NavBar />
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/users" exact component={Users} />
+        <Route path="/users/:userId?" exact component={Users} />
         <Route path="/login" exact component={Login} />
+        <Redirect to="/" />
       </Switch>
     </>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,24 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <>
       <ul className="nav">
         <li className="nav-item">
-          <NavLink exact to="/" className="nav-link" activeClassName="bg-light">
+          <Link className="nav-link" aria-current="page" to="/">
             Main
-          </NavLink>
+          </Link>
         </li>
         <li className="nav-item">
-          <NavLink to="/login" className="nav-link" activeClassName="bg-light">
+          <Link className="nav-link" aria-current="page" to="/login">
             Login
-          </NavLink>
+          </Link>
         </li>
         <li className="nav-item">
-          <NavLink to="/users" className="nav-link" activeClassName="bg-light">
+          <Link className="nav-link" aria-current="page" to="/users">
             Users
-          </NavLink>
+          </Link>
         </li>
       </ul>
     </>
