9763628fe5b5a789554203f2e38e9634ed80346b
musovvir <mr.muzagov@mail.ru>
2021-11-15 15:50:25 +0300

    add serveces

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,26 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Main from "./app/layouts/main";
 import Users from "./app/layouts/users";
 import Login from "./app/layouts/login";
+import { ToastContainer } from "react-toastify";
+import { ProfessionProvider } from "./app/hooks/useProfession";
 
 const App = () => {
     return (
         <>
             <NavBar />
             <Switch>
-                <Route path="/" exact component={Main} />
-                <Route path="/users/:userId?/:edit?" exact component={Users} />
-                <Route path="/login/:type?" exact component={Login} />
+                <ProfessionProvider>
+                    <Route path="/" exact component={Main} />
+                    <Route
+                        path="/users/:userId?/:edit?"
+                        exact
+                        component={Users}
+                    />
+                    <Route path="/login/:type?" exact component={Login} />
+                </ProfessionProvider>
                 <Redirect to="/" />
             </Switch>
+            <ToastContainer />
         </>
     );
 };
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
+import { UserProvider } from "../hooks/useUsers";
+
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
     return (
-        <>
+        <UserProvider>
             {userId ? (
                 edit ? (
                     <EditUserPage />
@@ -17,7 +19,7 @@ const Users = () => {
             ) : (
                 <UsersListPage />
             )}
-        </>
+        </UserProvider>
     );
 };
 
